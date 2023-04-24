86ee62b52a248ae754f6fdb02770ab66d2b13051
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-04-24 15:59:46 -0700

    A script that, uses a REST API, for a given employee ID,returns

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''A script that, uses a REST API, for a given employee ID,returns
+information about his/her TODO list progress.
+Usage:
+   ./0-gather_data_from_an_API.py <id>
+Author:
+    Abdulsalam Abdulsomad .A. - April 24th, 2023.
+'''
+import requests
+import sys
+
+
+API = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        ID = int(sys.argv[1])
+        if ID > 0:
+            # Get all todos
+            todos_req = requests.get(f"{API}/todos").json()
+            # gets the user with the id provided
+            user_req = requests.get(f"{API}/users/{ID}").json()
+            # retrieve name of user
+            user_name = user_req.get('name')
+            # get a list of all todos of the user
+            user_todos = []
+            for todo in todos_req:
+                if todo.get('userId') == ID:
+                    user_todos.append(todo)
+            # get a list of completed todos
+            completed_todo = []
+            for todo in user_todos:
+                if todo.get('completed'):
+                    completed_todo.append(todo)
+            print("Employee {} is done with tasks({}/{})"
+                  .format(
+                      user_name,
+                      len(completed_todo),
+                      len(user_todos)
+                      )
+                  )
+            for task in completed_todo:
+                print(f" \t{task.get('title')}")
