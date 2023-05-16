cae0333846f3f5e4c369edc151a26eb6338f0eaa
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-05-16 08:05:33 -0700

    Wrote a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    '''A  function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit
+    '''A  function that queries the Reddit API and returns the
+ number of subscribers (not active users, total subscribers) for a
+given subreddit
         parameters:
         ***********
             subreddit(str): subreddit to query.
@@ -18,9 +20,14 @@ def number_of_subscribers(subreddit):
             The number of subscribers.
         Raise:
         ******
-    
+             void
     '''
-    url = "https://www.reddit.com/r/{}/about".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     header = {'User-Agent': 'MyBot/1.0'}
-    response = requests.get(url, headers=header, allow_redirect=False)
-
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code != 200:
+        return (0)
+    else:
+        data = response.json()
+        subs = data['data']['subscribers']
+        return (subs)
